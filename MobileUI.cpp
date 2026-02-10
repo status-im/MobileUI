2b618213565f142de31b94ac2dd9aee8a709759a
Jonathan Rainville <rainville.jonathan@gmail.com>
2026-02-10 13:57:37 -0500

    set instance crash?

--- MobileUI.cpp
+++ MobileUI.cpp
@@ -68,7 +68,7 @@ void MobileUI::registerQML()
 
 MobileUI::MobileUI(QObject *parent) : QObject(parent)
 {
-    MobileUI::setInstance(this);
+    // MobileUI::setInstance(this);
 
 #if defined(Q_OS_ANDROID) || defined(Q_OS_IOS)
     QScreen *screen = qApp->primaryScreen();
